5d2e6a94cde86046301c03e5c46df54320028907
salonirana2018 <salonirana2018@gmail.com>
2026-04-20 17:38:37 +0545

    Create ChiyaPasal.java

---
+++ ChiyaPasal.java
@@ -0,0 +1,68 @@
+Skip to content
+saharsha-nep
+CS4001-Workshop
+Repository navigation
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security and quality
+Insights
+Commit 93be1c5
+author
+Saharsha
+committed
+2 weeks ago
+Week1 Scenario
+main
+0 parents  commit 
+93be1c5
+File tree
+Filter files…
+ChiyaPasal.java
+1 file changed
++23
+-0
+lines changed
+Search within code
+ 
+Customizable line height
+The default line height has been increased for improved accessibility. You can choose to enable a more compact line height from the view settings menu.
+
+‎ChiyaPasal.java‎
++23
+Lines changed: 23 additions & 0 deletions
+Original file line number	Diff line number	Diff line change
+@@ -0,0 +1,23 @@
+public class ChiyaPasal{
+    public static void main(String[] args){
+        System.out.println("====================================");
+        System.out.println("       Welcome to Chiya Pasal!      ");
+        System.out.println("====================================");
+        System.out.println("");
+        System.out.println("-----------Special Items------------");
+        System.out.println("- Chiya(Tea)");
+        System.out.println("- Samosa");
+        System.out.println("- Pakoda");
+        System.out.println("------------------------------------");
+        System.out.println("");
+        System.out.println("---------------Hours----------------");
+        System.out.println("Open from 5:00 AM to 9:00 pm");
+        System.out.println("------------------------------------");
+        System.out.println("");
+        System.out.println("--------Contact and Location--------");
+        System.out.println("Contact: +977-1234567890");
+        System.out.println("Location: Biratnagar,Nepal");
+        System.out.println("------------------------------------");
+        
+    }
+}
+0 commit comments
+Comments
+0
+ (0)
+Comment
+You're not receiving notifications from this thread.
+
+q
